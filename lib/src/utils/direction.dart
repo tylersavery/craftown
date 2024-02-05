c313f9d791ddc3fb2e6508ea5e1e853c93846973
Tyler Savery <tyler@theyoungastronauts.com>
2024-02-05 16:49:39 -0500

    tweak to getWalkDirectionForMining utility function

--- lib/src/utils/direction.dart
+++ lib/src/utils/direction.dart
@@ -1,20 +1,24 @@
 import 'package:craftown/src/components/player.dart';
 import 'package:flame/components.dart';
 
-//TODO: fix this logic, its a bit wonky
 WalkDirection getWalkDirectionForMining(Player player, PositionComponent target) {
   final playerCenter = Vector2(player.position.x + (player.size.x / 2), player.position.y + (player.size.y / 2));
-  final resourceCenter = Vector2(target.position.x + (target.size.x / 2), target.position.y + (target.y / 2));
 
-  final deltaX = playerCenter.x - resourceCenter.x;
-  final deltaY = playerCenter.y - resourceCenter.y;
-  if (deltaX > 0 && deltaX > deltaY) {
-    return WalkDirection.left;
-  } else if (deltaX < 0 && deltaX < deltaY) {
+  final targetL = target.position.x;
+  final targetR = target.position.x + target.size.x;
+  final targetT = target.position.y;
+
+  if (playerCenter.x < targetL) {
     return WalkDirection.right;
-  } else if (deltaY > 0) {
-    return WalkDirection.up;
-  } else {
-    return WalkDirection.down;
   }
+
+  if (playerCenter.x > targetR) {
+    return WalkDirection.left;
+  }
+
+  if (playerCenter.y > targetT) {
+    return WalkDirection.up;
+  }
+
+  return WalkDirection.down;
 }
